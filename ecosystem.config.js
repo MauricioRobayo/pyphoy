f8af99f75a421c05fe7b9ba4678bd7764fd0bebb
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-09-21 15:18:21 -0500

    chore: update path on production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       port: process.env.PYPHOY_PRODUCTION_PORT,
       ref: "origin/master",
       repo: "git@github.com:archemiro/pyphoy.git",
-      path: "/home/pyp/pyphoy",
+      path: "/home/node/pyphoy",
       "post-deploy":
         "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
     }
