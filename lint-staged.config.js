d88bbabfd6f71ca97f6227be25a459189a7ef3f8
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-10-22 12:57:37 -0500

    chore: change pretty-quick for lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  linters: {
+    "src/**/*.js": [
+      "eslint --fix",
+      "git add"
+      // "jest --bail --coverage --findRelatedTests"
+    ],
+    "**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}": [
+      "prettier --write",
+      "git add"
+    ]
+  },
+  ignore: ["dist"]
+};
