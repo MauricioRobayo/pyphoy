68b6d657cd81e6bc78422eacbfd8291f86440cef
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-12-23 16:03:07 -0500

    chore: update repo url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       host: process.env.PYPHOY_PRODUCTION_HOST,
       port: process.env.PYPHOY_PRODUCTION_PORT,
       ref: "origin/master",
-      repo: "git@github.com:archemiro/pyphoy.git",
+      repo: "git@github.com:picoyplaca/pyphoy.git",
       path: "/home/pyphoy/pyphoy",
       "post-deploy":
         "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
