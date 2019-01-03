b0abdc305760d6fab8234dfc9ef0056bad4de43c
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2019-01-03 17:49:06 -0500

    chore(pyptron): add dev and prod conf

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,10 @@
+const http = require("http");
 const https = require("https");
 
 exports.request = params =>
   new Promise((resolve, reject) => {
-    const req = https.request(params, res => {
+    const { get } = params.protocol === "http:" ? http : https;
+    const req = get(params, res => {
       if (res.statusCode < 200 || res.statusCode >= 300) {
         reject(new Error(res.statusCode));
         return;
