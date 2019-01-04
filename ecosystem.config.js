3f22ba5fcff7bd57ec67a27b0df75307379c0e4a
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2019-01-03 20:51:29 -0500

    ci: add development environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,14 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3246
       }
+    },
+    {
+      name: "pyphoy-dev",
+      script: "src/bin/server.js",
+      env_development: {
+        NODE_ENV: "production",
+        PORT: 3247
+      }
     }
   ],
   deploy: {
@@ -18,6 +26,17 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:picoyplaca/pyphoy.git",
       path: "/home/pyphoy/pyphoy",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
+    },
+    development: {
+      user: process.env.PYPHOY_PRODUCTION_USER,
+      host: process.env.PYPHOY_PRODUCTION_HOST,
+      port: process.env.PYPHOY_PRODUCTION_PORT,
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/development",
+      repo: "git@github.com:archemiro/pyphoy.git",
+      path: "/home/pyphoy/pyphoy-dev",
       "post-deploy":
         "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
     }
