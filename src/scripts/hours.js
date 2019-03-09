5830fa977679d919f4002ac76bd5d8461f67cf30
Mauricio Robayo <rfmajo@gmail.com>
2019-03-09 11:20:39 -0500

    style: update prettier format rules

--- src/scripts/hours.js
+++ src/scripts/hours.js
@@ -1,52 +1,52 @@
 function timeString(time) {
-  const hours = Math.floor(time / (1000 * 60 * 60));
-  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  return `${hours ? `${hours}h` : ""}${minutes ? ` ${minutes}m` : ""}`;
+  const hours = Math.floor(time / (1000 * 60 * 60))
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+  return `${hours ? `${hours}h` : ''}${minutes ? ` ${minutes}m` : ''}`
 }
 
-const uls = document.getElementsByClassName("hours-interactive");
+const uls = document.getElementsByClassName('hours-interactive')
 
 for (let i = 0; i < uls.length; i += 1) {
-  const ul = uls[i];
+  const ul = uls[i]
 
-  const lis = ul.getElementsByTagName("li");
-  const { comment } = ul.dataset;
-  const { days } = ul.dataset;
+  const lis = ul.getElementsByTagName('li')
+  const { comment } = ul.dataset
+  const { days } = ul.dataset
 
   for (let j = 0; j < lis.length; j += 1) {
-    const li = lis[j];
+    const li = lis[j]
 
-    const { start } = li.dataset;
-    const { end } = li.dataset;
+    const { start } = li.dataset
+    const { end } = li.dataset
 
-    const today = new Date();
-    const dtString = today.toDateString();
+    const today = new Date()
+    const dtString = today.toDateString()
     const startTime = new Date(
-      `${dtString} ${comment === "Todo el día" ? "0:00" : start}`
-    );
+      `${dtString} ${comment === 'Todo el día' ? '0:00' : start}`
+    )
     const endTime = new Date(
-      `${dtString} ${comment === "Todo el día" ? "0:00" : end}`
-    );
-    if (endTime <= startTime) endTime.setDate(endTime.getDate() + 1);
-    const hourActive = today >= startTime && today <= endTime;
-    const hourWarning = today < startTime;
+      `${dtString} ${comment === 'Todo el día' ? '0:00' : end}`
+    )
+    if (endTime <= startTime) endTime.setDate(endTime.getDate() + 1)
+    const hourActive = today >= startTime && today <= endTime
+    const hourWarning = today < startTime
 
     if (
       (hourActive || hourWarning) &&
       (days.length === 0 || days.includes(today.getDay()))
     ) {
-      const span = document.createElement("span");
+      const span = document.createElement('span')
       const textNode = document.createTextNode(
         hourActive ? timeString(endTime - today) : timeString(startTime - today)
-      );
+      )
 
-      span.classList.add("lapse");
-      span.appendChild(textNode);
+      span.classList.add('lapse')
+      span.appendChild(textNode)
 
-      li.classList.add(`hour-${hourActive ? "active" : "warning"}`);
-      li.appendChild(span);
+      li.classList.add(`hour-${hourActive ? 'active' : 'warning'}`)
+      li.appendChild(span)
 
-      break;
+      break
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,24 +1,24 @@
-const http = require("http");
-const https = require("https");
+const http = require('http')
+const https = require('https')
 
 exports.request = params =>
   new Promise((resolve, reject) => {
-    const { get } = params.protocol === "http:" ? http : https;
+    const { get } = params.protocol === 'http:' ? http : https
     const req = get(params, res => {
       if (res.statusCode < 200 || res.statusCode >= 300) {
-        reject(new Error(res.statusCode));
-        return;
+        reject(new Error(res.statusCode))
+        return
       }
-      let data = "";
-      res.on("data", _data => {
-        data += _data;
-      });
-      res.on("end", () => {
-        resolve(JSON.parse(data));
-      });
-    });
-    req.on("error", err => {
-      reject(err);
-    });
-    req.end();
-  });
+      let data = ''
+      res.on('data', _data => {
+        data += _data
+      })
+      res.on('end', () => {
+        resolve(JSON.parse(data))
+      })
+    })
+    req.on('error', err => {
+      reject(err)
+    })
+    req.end()
+  })
