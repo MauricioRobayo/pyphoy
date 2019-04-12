d27413098f8907ebf6fd9b59b466bd6f0b40e598
Mauricio Robayo <rfmajo@gmail.com>
2019-04-12 12:13:34 -0500

    chore: split dev/prod pm2 environments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,9 @@ module.exports = {
     {
       name: 'pyphoy',
       script: 'src/bin/server.js',
-      env_production: {
+      env: {
         NODE_ENV: 'production',
         PORT: 3246,
-      },
-    },
-    {
-      name: 'pyphoy-dev',
-      script: 'src/bin/server.js',
-      env_development: {
-        NODE_ENV: 'production',
-        PORT: 3247,
       },
     },
   ],
@@ -25,18 +17,18 @@ module.exports = {
       ssh_options: 'StrictHostKeyChecking=no',
       ref: 'origin/master',
       repo: 'git@github.com:picoyplaca/pyphoy.git',
-      path: '/home/pyphoy/pyphoy',
+      path: `/home/${process.env.PYPHOY_PRODUCTION_USER}/pyphoy`,
       'post-deploy':
         'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save',
     },
     development: {
-      user: process.env.PYPHOY_PRODUCTION_USER,
-      host: process.env.PYPHOY_PRODUCTION_HOST,
-      port: process.env.PYPHOY_PRODUCTION_PORT,
+      user: process.env.PYPHOY_DEVELOPMENT_USER,
+      host: process.env.PYPHOY_DEVELOPMENT_HOST,
+      port: process.env.PYPHOY_DEVELOPMENT_PORT,
       ssh_options: 'StrictHostKeyChecking=no',
       ref: 'origin/develop',
       repo: 'git@github.com:archemiro/pyphoy.git',
-      path: '/home/pyphoy/pyphoy-dev',
+      path: `/home/${process.env.PYPHOY_DEVELOPMENT_USER}/pyphoy`,
       'post-deploy':
         'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env development && pm2 save',
     },
