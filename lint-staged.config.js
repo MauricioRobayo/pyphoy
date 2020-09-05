591a166b1a769f67550eb562ae8c154a7b1fe19c
Mauricio Robayo <rfmajo@gmail.com>
2020-09-05 06:53:00 -0500

    chore: remove "git add" from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   'src/**/*.js': [
     'eslint --fix',
-    'git add',
     // "jest --bail --coverage --findRelatedTests"
   ],
-  '**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}': [
-    'prettier --write',
-    'git add',
-  ],
+  '**/*.{js,ts,css,less,scss,vue,json,gql,md,yml,yaml}': ['prettier --write'],
 }
