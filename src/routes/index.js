bf382f246ce798cc1923c59d93acdaa030c27563
Mauricio Robayo <rfmajo@gmail.com>
2020-10-23 09:18:16 -0500

    chore: update sitemap.xml with new numbers route

--- src/routes/index.js
+++ src/routes/index.js
@@ -44,17 +44,19 @@ router.get('/sitemap.xml', (req, res, next) => {
           city === 'manizales' &&
           category === 'transporte-publico-colectivo'
         ) {
-          ['H', 'I', 'J', 'A', 'B', 'C', 'D', 'E', 'F', 'G'].forEach((num) => {
-            smStream.write({
-              url: `/${city}/${category}/${num}`,
-              changefreq: 'daily',
-              priority: 0.2,
-            });
-          });
+          ['H', 'I', 'J', 'A', 'B', 'C', 'D', 'E', 'F', 'G'].forEach(
+            (letter) => {
+              smStream.write({
+                url: `/${city}/${category}/placa/${letter}`,
+                changefreq: 'daily',
+                priority: 0.2,
+              });
+            }
+          );
         } else {
           for (let i = 0; i <= 9; i += 1) {
             smStream.write({
-              url: `/${city}/${category}/${i}`,
+              url: `/${city}/${category}/placa/${i}`,
               changefreq: 'daily',
               priority: 0.2,
             });
