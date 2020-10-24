2bd89e7d9fcac0128cff7ebcdd4f3e0f2b23c4b8
Mauricio Robayo <rfmajo@gmail.com>
2020-10-24 06:05:04 -0500

    chore: remove all unnecessary files

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  env: {
-    browser: true,
-    jest: true,
-  },
-  ignorePatterns: ['assets/js/'],
-};
--- stylelint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'stylelint-config-recommended',
-  plugins: ['stylelint-scss'],
-  rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
-  },
-  ignoreFiles: ['assets/**/*'],
-};
