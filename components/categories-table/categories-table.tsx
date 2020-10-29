adce8d96ab24cb915f1ea217489b4a21bd9d61eb
Mauricio Robayo <rfmajo@gmail.com>
2020-10-29 09:21:27 -0500

    refactor: display logic for category table

--- components/categories-table/categories-table.tsx
+++ components/categories-table/categories-table.tsx
@@ -18,17 +18,19 @@ function isPublicLicense(categoryName: string) {
   return lowerCaseName === 'taxis' || lowerCaseName.includes('público');
 }
 
+const ALL_DIGITS = 'Todos';
+
 function hasAllDigits(numbers: number[]) {
   return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].every((num) => numbers.includes(num));
 }
 
 function pypNumbersToString(numbers: number[]) {
   if (numbers.length === 0) {
-    return 'No Aplica';
+    return 'No aplica';
   }
 
   if (hasAllDigits(numbers)) {
-    return 'TODOS';
+    return ALL_DIGITS;
   }
 
   return numbers.join('-');
@@ -45,17 +47,29 @@ export default function CategoriesTable({ categories }: CategoryTableProps) {
       {categoriesData.map(
         ({ name: categoryName, data: [{ numbers, scheme, hours }] }) => {
           const numbersString = pypNumbersToString(numbers);
+          const allDigits = numbersString === ALL_DIGITS;
+          const hasRestriction = numbers.length > 0;
+
           return (
             <article key={categoryName} className={styles.categoryRow}>
               <h4 className={styles.categoryTitle}>{categoryName}</h4>
-              <Hours className={utilStyles.mx_1} hours={hours} interactive />
+              {hasRestriction ? (
+                <div>
+                  <h5>Horario</h5>
+                  <Hours
+                    className={utilStyles.mx_1}
+                    hours={hours}
+                    interactive
+                  />
+                </div>
+              ) : null}
               <LicensePlate
                 className={utilStyles.mt_1}
                 numbers={numbersString}
                 publicLicense={isPublicLicense(categoryName)}
                 size="big"
               />
-              {numbersString === 'TODOS' ? null : (
+              {allDigits || !hasRestriction ? null : (
                 <div>
                   {`${
                     scheme === Scheme.FirstNumber
